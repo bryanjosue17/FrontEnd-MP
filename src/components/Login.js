ff53e0d2103c8bb48bf9331b7eb0b0bafff3a81b
bryanjosue17 <xolbryan@gmail.com>
2022-02-07 21:46:18 -0600

    +Se agrego la pantalla de inicio

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,22 @@
 import React from "react";
+import logo from "../assets/logo.png"; 
 
 const Login = () => {
   return (
-    <div>
-      <h3>Debes iniciar sesion para continuar</h3>
+    <div
+      style={{
+        justifyContent: "center",
+        alignItems: "center",
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <img
+        style={{ width: "50%", height: "50%", marginTop: "5%" }}
+        src={logo}
+        alt="Logo"
+      />
+      <h3 style={{ marginTop: "5%" }}>Debes iniciar sesión para continuar</h3>
     </div>
   );
 };
